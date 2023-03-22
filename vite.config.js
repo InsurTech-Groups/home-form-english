fb4cfe1c63faa5826aee61f2525743fc63d0e92d
David <david@tangentdigitalagency.com>
2023-03-21 21:14:22 -0700

    updates to vite

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,11 @@ import tailwindcss from 'tailwindcss'
 export default defineConfig({
   plugins: [react()],
 
+  preview: {
+    port: 3000,
+    open: true,
+  },
+
   optimizeDeps: {
     include: ['linked-dep'],
   },
