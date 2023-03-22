91a41fe0b46446758320ca20376134f43ef1314d
David <david@tangentdigitalagency.com>
2023-03-21 21:59:03 -0700

    final build

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect} from "react";
 import "../styles/LandingPage.css";
-import LandingHeaderPic from "../assets/HeaderImage.png";
+import LandingHeaderPic from "../assets/HeaderImage.jpg";
 import { BoltIcon, GlobeAltIcon, ScaleIcon } from '@heroicons/react/24/outline';
 import ZipCode from '../apis/zipCode'
 import FAQ from "../components/faq";
@@ -44,24 +44,24 @@ function LandingPage() {
   return (
     <div className="bg-dark-purple">
       <main className="lg:relative">
-        <div className="mx-auto w-full max-w-7xl pt-16 pb-20 text-center lg:py-48 lg:text-left">
+        <div className="w-full pt-16 pb-20 mx-auto text-center max-w-7xl lg:py-48 lg:text-left">
           <div className="px-4 sm:px-8 lg:w-1/2 xl:pr-16">
             <h1 className="text-3xl font-bold tracking-tight text-white sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl">
               <span className="block xl:inline">Get Your Free</span> <br />
-              <span className="block font-extrabold text-transparent text-6xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 xl:inline">
+              <span className="block text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 xl:inline">
                 {formType} {' '}
               </span>
               Quote
             </h1>
-            <p className="mx-auto mt-3 max-w-md text-lg text-white sm:text-xl md:mt-5 md:max-w-3xl">
+            <p className="max-w-md mx-auto mt-3 text-lg text-white sm:text-xl md:mt-5 md:max-w-3xl">
               Fill out a form in less than 3 minutes, and talk to an agent to get a quote tailored for you at a competitive price. Shopping for {formType} has never been easier.
             </p>
               <ZipCode />
           </div>
         </div>
-        <div className="relative h-64 w-full sm:h-72 md:h-96 lg:absolute lg:inset-y-0 lg:right-0 lg:h-full lg:w-1/2">
+        <div className="relative w-full h-64 sm:h-72 md:h-96 lg:absolute lg:inset-y-0 lg:right-0 lg:h-full lg:w-1/2">
           <img
-            className="absolute inset-0 h-full w-full object-cover"
+            className="absolute inset-0 object-cover w-full h-full"
             src={LandingHeaderPic}
             alt=""
           />
@@ -69,14 +69,14 @@ function LandingPage() {
       </main>
 
       <div className="bg-input-purple py-36">
-        <div className="mx-auto max-w-xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
-        <h1 className=" text-center mb-12 text-4xl font-bold tracking-tight text-white sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl">Why Choose Us </h1>
+        <div className="max-w-xl px-4 mx-auto sm:px-6 lg:max-w-7xl lg:px-8">
+        <h1 className="mb-12 text-4xl font-bold tracking-tight text-center text-white  sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl">Why Choose Us </h1>
         <dl className="space-y-10 lg:grid lg:grid-cols-3 lg:gap-8 lg:space-y-0">
           {features.map((feature) => (
-            <div key={feature.name} className='bg-white rounded-xl p-5'>
+            <div key={feature.name} className='p-5 bg-white rounded-xl'>
               <dt>
-                <div className="flex h-12 w-12 items-center justify-center rounded-md bg-button-purple text-white">
-                  <feature.icon className="h-6 w-6" aria-hidden="true" />
+                <div className="flex items-center justify-center w-12 h-12 text-white rounded-md bg-button-purple">
+                  <feature.icon className="w-6 h-6" aria-hidden="true" />
                 </div>
                 <p className="mt-5 text-lg font-extrabold leading-6 text-dark-purple">{feature.name}</p>
               </dt>
